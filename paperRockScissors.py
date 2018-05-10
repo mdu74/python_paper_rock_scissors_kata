fdd904ad0632b99567ad3a47ba6f47b69fd2d256
Mdu <mdu74.C@gmail.com>
2018-05-10 17:49:14 +0200

    reduced to only two results

--- paperRockScissors.py
+++ paperRockScissors.py
@@ -1,14 +1,6 @@
 class PaperRockScissors(object):
     def Play(myMove, opponentsMoves):
-        if myMove == "Sciccors" and opponentsMoves == "Rock":
+        if (myMove == "Sciccors" and opponentsMoves == "Rock") or (myMove == "Paper" and opponentsMoves == "Sciccors") or (myMove == "Rock" and opponentsMoves == "Paper"):
             return "Loose"
-        elif myMove == "Paper" and opponentsMoves == "Sciccors":
-            return "Loose"
-        elif myMove == "Rock" and opponentsMoves == "Paper":
-            return "Loose"
-        elif myMove == "Rock" and opponentsMoves == "Sciccors":
-            return "Win"
-        elif myMove == "Sciccors" and opponentsMoves == "Paper":
-            return "Win"
-        elif myMove == "Paper" and opponentsMoves == "Rock":
+        elif (myMove == "Rock" and opponentsMoves == "Sciccors") or (myMove == "Sciccors" and opponentsMoves == "Paper") or (myMove == "Paper" and opponentsMoves == "Rock"):
             return "Win"
